3cc4de3bf4e4b1c9398352d22b266bf39716481c
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2019-09-05 11:45:27 +0530

    day#2-update#4

--- javascript-demos/es2015-demos-2/search-array-es5.js
+++ javascript-demos/es2015-demos-2/search-array-es5.js
@@ -1,8 +1,25 @@
 
+function isPrime(number){
+    if(number<2) return false;
+    for(let i=2;i<number;i++)
+        if(number%2==0)
+            return false;
+
+    return true;
+}
+
+function searchEvens(values){
+    let result=[];
+
+    for(let value of values)
+        if(value%2===0)
+            result.push(value);
+
+    return result;
+}
 
 let original=[2,3,9,24,11,7,15,24,29,37,55,62,82,71];
-
+console.log('original',original);
 
 let evens= searchEvens(original);
-
 console.log('evens',evens);
